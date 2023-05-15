26388914808982e1cc3f5946b216b2903f76efd1
myhyun0002 <a01089417552@gmail.com>
2023-05-15 18:04:22 +0900

    [fix] 수정

--- app/app.py
+++ app/app.py
@@ -82,7 +82,7 @@ def register_and_broadcast_node():
 @app.route('/register-node', methods=['POST']) # 새로운 노드를 연결하는 요청 받은 노드가 원래 연결되어 있던 노드에게 새로운 노드를 등록하는 요청 보내는 API
 def register_node():
     new_node_url = request.json['newNodeUrl']
-    node_not_already_present = (i != new_node_url for i in bitcoin.network_nodes)  #채우시오 : new_node_url이 network_noeds에 없으면 true (type boolean)
+    node_not_already_present = new_node_url not in bitcoin.newwork_nodes  #채우시오 : new_node_url이 network_noeds에 없으면 true (type boolean)
     not_current_node = bitcoin.current_node_url != new_node_url  #채우시오 : current_node_url이 new_node_url이 아니면 true(type boolean)
     if node_not_already_present and not_current_node:#두 가지 조건을 모두 만족하면 실행
         #새로운 노드 network_node에 추가
@@ -94,7 +94,7 @@ def register_node():
 def register_nodes_bulk():
     all_network_nodes = request.json['allNetworkNodes']
     for network_node_url in all_network_nodes:
-        node_not_already_present = (i != network_node_url for i in bitcoin.network_nodes) #채우시오 : new_node_url이 network_noeds에 없으면 true (type boolean)
+        node_not_already_present = network_node_url not in bitcoin.network_nodes  #채우시오 : new_node_url이 network_noeds에 없으면 true (type boolean)
         not_current_node = bitcoin.current_node_url != network_node_url #채우시오 : current_node_url이 new_node_url이 아니면 true(type boolean)
         if node_not_already_present and not_current_node: #두 가지 조건을 모두 만족하면 실행
             #새로운 노드 network_node에 추가
